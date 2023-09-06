6573f8d034ae6ec6e67c79f53939c9ed616a5c6f
nazarkubyk <kubik.nazar.work@gmail.com>
2023-09-06 17:45:52 +0300

    created navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import {NavBar} from "./components/index";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>TRELLO CLONE</h3>
-    </main>
-  )
+  return <NavBar />
 }
